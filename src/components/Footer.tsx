8920f4b0582e7c7f81d0ffdc589393ea7cb774b9
Cherry <Cherryworanat@gmail.com>
2025-08-16 13:11:49 +0700

    sg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
-export default function Footer() {
+import { type FooterProps } from "../libs/Footer";
+
+export default function Footer({ year, fullName, studentId }: FooterProps) {
   return (
     <footer className="align-self-end text-center w-100">
       <p className="text-white bg-secondary p-4 m-0">
-        Copyright © 2568 Woranat Thammawit 670612130
+        Copyright © {year} {fullName} {studentId}
       </p>
     </footer>
   );
